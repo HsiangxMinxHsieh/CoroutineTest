164bb85d46323b19a861806bf53f6a0781760042
timmymike <t19880916@gmail.com>
2020-09-03 11:16:26 +0800

    commit record 02

--- app/src/main/java/com/timmymike/coroutinetest/MainActivity.kt
+++ app/src/main/java/com/timmymike/coroutinetest/MainActivity.kt
@@ -9,40 +9,58 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private var job: Job? = null
+    private var job1: Job? = null
+    private var job2: Job? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        job = fun1()
-
     }
-    private fun fun1():Job{
+
+    private fun fun1(): Job {
         return GlobalScope.launch {
             launch {
-                repeat(1000) { i ->
+                repeat(100) { i ->
                     println("job1: I'm sleeping $i ...")
-                    delay(500L)
+                    delay(50L)
                 }
             }
-            fun2()
 
+            if (fun2()) {
+                println("fun2 is return true")
+            } else {
+                println("fun2 is return false")
+            }
+            println("OK,done.")
         }
     }
 
-    private fun fun2() {
-        repeat(1000) { i ->
-            println("job2: I'm sleeping $i ...")
-            Thread.sleep(500L)
+    private fun fun2(): Boolean {
+        job2 = GlobalScope.launch {
+            repeat(100) { i ->
+                println("job2: I'm sleeping $i ...")
+                delay(50L)
+            }
         }
 
+        while (job2?.isCompleted == false) {
+            Thread.sleep(10)
+        }
+        return (0..1).random() == 1
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        job1 = fun1()
     }
 
     override fun onPause() {
         super.onPause()
         GlobalScope.launch {
-            println("main: I'm tired of waiting!")
-            job?.cancel() // cancels the job
-            job?.join() // waits for job's completion
+            println("main: I'm tired of waiting for job1")
+            job1?.cancel() // cancels the job
+            println("main: I'm tired of waiting for job2")
+            job2?.cancel() // cancels the job
             println("main: Now I can quit.")
         }
     }
